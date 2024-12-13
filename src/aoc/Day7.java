e954fbbc961f931782d7220fe0580d65be293e96
Nathan Miles <nathan.miles95@gmail.com>
2024-12-13 18:38:20 -0500

    Day 7 Part 2

--- src/aoc/Day7.java
+++ src/aoc/Day7.java
@@ -1,13 +1,32 @@
 package aoc;
 
+import static java.lang.Integer.parseInt;
 import static java.lang.Long.parseLong;
 import static java.util.Arrays.stream;
 
 import java.util.List;
+import java.util.function.BiFunction;
 
 public class Day7 implements Day {
     @Override
     public long part1(List<String> input) {
+        return sumOfSatisfyable(input,
+                (a, b) -> a + b,
+                (a, b) -> a * b
+        );
+    }
+
+    @Override
+    public long part2(List<String> input) {
+        return sumOfSatisfyable(input,
+                (a, b) -> a + b,
+                (a, b) -> a * b,
+                (a, b) -> parseLong(a + "" + b)
+        );
+    }
+
+    @SafeVarargs
+    private long sumOfSatisfyable(List<String> input, BiFunction<Long, Long, Long>... operations) {
         return input.stream()
                 .map(line -> {
                     String[] sides = line.split(": ");
@@ -17,31 +36,33 @@ public class Day7 implements Day {
                             stream(sides[1].split(" ")).map(Long::parseLong).toList()
                     );
                 })
-                .filter(Equation::isPart1Satisfyable)
+                .filter(equation -> equation.isSatisfyable(operations))
                 .mapToLong(Equation::left)
                 .sum();
     }
 
-    @Override
-    public long part2(List<String> input) {
-        return 0;
-    }
-
     private record Equation(long left, List<Long> right) {
 
-        boolean isPart1Satisfyable() {
-            long magicLimit = (1L << (right.size() - 1)) - 1;
-            for (int magic = 0; magic <= magicLimit; magic++) {
+        // Works by encoding each operation as a digit in an integer with base equal to the number of operations
+        @SafeVarargs
+        private boolean isSatisfyable(BiFunction<Long, Long, Long>... operations) {
+            int radix = operations.length;
+            int lastSequence = parseInt(Long.toString(radix - 1).repeat(right.size() - 1), radix);
+
+            for (int operationSequence = 0; operationSequence <= lastSequence; operationSequence++) {
+                String sequenceString = Integer.toString(operationSequence, radix);
+                sequenceString = "0".repeat(right.size() - sequenceString.length() - 1) + sequenceString;
+
                 long total = right.getFirst();
-                for (int i = 0; i < right.size() - 1; i++) {
-                    long nextNumber = right.get(i + 1);
-                    total = (magic & (1L << i)) == 0 ? total + nextNumber : total * nextNumber;
+                for (int i = 1; i < right.size(); i++) {
+                    total = operations[sequenceString.charAt(i - 1) - '0'].apply(total, right.get(i));
                 }
 
                 if (total == left) {
                     return true;
                 }
             }
+
             return false;
         }
     }
