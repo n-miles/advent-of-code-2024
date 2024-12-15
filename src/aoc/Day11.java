5441d327515284452ff12e3926351c5c9d6fdfe7
Nathan Miles <nathan.miles95@gmail.com>
2024-12-15 12:26:25 -0500

    Fixed Day 11 performance

--- src/aoc/Day11.java
+++ src/aoc/Day11.java
@@ -1,50 +1,63 @@
 package aoc;
 
 import static java.lang.Long.parseLong;
-import static java.util.Arrays.stream;
 
+import java.util.HashMap;
 import java.util.List;
-import java.util.stream.LongStream;
+import java.util.Map;
 
 public class Day11 implements  Day {
 
     @Override
     public long part1(List<String> input) {
-        return stream(input.getFirst().split(" "))
-                .mapToLong(Long::parseLong)
-                .flatMap(rock -> afterBlinks(rock, 25))
-                .count();
+        Map<Pair<Long, Integer>, Long> cache = new HashMap<>();
+        long sum = 0;
+        for (String rock : input.getFirst().split(" ")) {
+            sum += countRocks(parseLong(rock), 25, cache);
+        }
+        return sum;
     }
 
     @Override
     public long part2(List<String> input) {
-        if (true) {
-            throw new RuntimeException("This will oom lol");
+        Map<Pair<Long, Integer>, Long> cache = new HashMap<>();
+        long sum = 0;
+        for (String rock : input.getFirst().split(" ")) {
+            sum += countRocks(parseLong(rock), 75, cache);
         }
-        return stream(input.getFirst().split(" "))
-                .mapToLong(Long::parseLong)
-                .flatMap(rock -> afterBlinks(rock, 75))
-                .count();
+        return sum;
     }
 
-    private LongStream afterBlinks(long rock, long remainingBlinks) {
-        if (remainingBlinks == 0) {
-            return LongStream.of(rock);
+    private long countRocks(long rock, int blinksRemaining, Map<Pair<Long, Integer>, Long> cache) {
+        if (blinksRemaining == 0) {
+            return 1;
         }
-        remainingBlinks--;
-
-        if (rock == 0) {
-            return afterBlinks(1, remainingBlinks);
-        }
-
-        String rockDigits = "" + rock;
-        if (rockDigits.length() % 2 == 0) {
-            return LongStream.concat(
-                    afterBlinks(parseLong(rockDigits.substring(0, rockDigits.length() / 2)), remainingBlinks),
-                    afterBlinks(parseLong(rockDigits.substring(rockDigits.length() / 2)), remainingBlinks)
-            );
+        Pair<Long, Integer> currentPosition = new Pair<>(rock, blinksRemaining);
+        Long cached = cache.get(currentPosition);
+        if (cached != null) {
+            return cached;
         }
 
-        return afterBlinks(rock * 2024, remainingBlinks);
+        blinksRemaining--;
+
+        // DIY lexical scope that can yield
+        long result = switch(0) {default -> {
+            if (rock == 0) {
+                yield countRocks(1, blinksRemaining, cache);
+            }
+
+            long digits = (long) (Math.log10(rock) + 1);
+            if (digits % 2 == 0) {
+                long dividend = (long) Math.pow(10, digits / 2);
+                long left = rock / dividend;
+                long right = rock % dividend;
+                yield countRocks(left, blinksRemaining, cache) + countRocks(right, blinksRemaining, cache);
+            }
+
+            yield countRocks(rock * 2024, blinksRemaining, cache);
+        }};
+
+        cache.put(new Pair<>(rock, blinksRemaining + 1), result);
+        return result;
     }
 }
